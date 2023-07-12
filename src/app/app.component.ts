22b361438379dc14b583b8698f8c55cbb328eac2
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-07-12 12:23:13 -0300

    Binding custom properties: Passing values from parent component to child component. (equivalent to vues props)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    {
+      type: 'server',
+      name: 'TestServer',
+      content: 'Just a test!'
+    },
+    {
+      type: 'server',
+      name: 'TestServer 2',
+      content: 'Just a test! 2'
+    }
+  ];
 }
