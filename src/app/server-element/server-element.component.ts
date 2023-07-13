980bfe22b638ce301444a5d2317a6e7d033c903b
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-07-13 20:42:11 -0300

    ViewEncapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // Native has the same effect as None, but it uses the ShadowDom instead. Emulated is the default value
+  encapsulation: ViewEncapsulation.Emulated // None, Native 
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {
